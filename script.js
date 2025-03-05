34bdf6668c917b0f3bfbb09b97d858a1ba46e945
student-Umeshkumar <umeshjha7890@gmail.com>
2025-03-05 20:56:07 +0530

    half code

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const searchButton = document.getElementById("search-btn");
+    const usernameInput = document.getElementById("user-input");
+    const statsContainer = document.querySelector(".stats-container");
+    const easyProgressCircle = document.querySelector(".easy-progress");
+    const mediumProgressCircle = document.querySelector(".medium-progress");
+    const hardProgressCircle = document.querySelector(".hard-progress");
+    const easyLabel = document.getElementById("easy-label");
+    const mediumLabel = document.getElementById("medium-label");
+    const hardLabel = document.getElementById("hard-label");
+    const cardStatsContainer = document.querySelector(".stats-cards");
+
+    //return true or false based on a regex
+    searchButton.addEventListener('click', function(){
+        const username = usernameInput.value;
+        console.log("loggin username:", username
+            
+        );
+        
+    })
+})
